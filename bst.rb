c71dd752d738bc54e296829675e4db3f0effdf55
Roman Geathers <romangeathersdev@gmail.com>
2022-06-30 16:57:37 -0400

    Add methods to build and print out binary tree

--- bst.rb
+++ bst.rb
@@ -1,24 +1,52 @@
 class Node
-  def initialize
-    @data = nil
-    @left_children = nil
-    @right_children = nil
+  include Comparable
+
+  attr_accessor :data, :left, :right
+
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+
+  def <=> (other)
+    data <=> other.data
   end
 end
 
+
+
+
+
+
+
 class Tree
+  attr_accessor :root
+
   def initialize(arr)
-    @arr = arr
-    @root = nil
+    @arr = arr.sort.uniq
+    @root = build_tree(@arr, 0, @arr.length - 1)
   end
 
-  def build_tree(arr = @arr)
-    arr = arr.sort
-    arr = arr.uniq
-    print arr
+  def build_tree(arr, start, ending)
+    return nil if start > ending
+
+    mid = (start + ending) / 2
+    node = Node.new(arr[mid])
+
+    node.left = build_tree(arr, start, mid - 1)
+    node.right = build_tree(arr, mid + 1, ending)
+
+    node
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
 
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 bst = Tree.new(array)
-bst.build_tree
+bst.pretty_print
